c9f101a6950b26dd2f0d11996445c89296a8a8ee
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-09-19 02:43:02 -0700

    main component

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Actions from "./lib/Actions.svelte";
+  import Output from "./lib/Output.svelte";
+</script>
+
+<main>
+  <section class="content">
+    <Output />
+  </section>
+  <section class="actions">
+    <Actions />
+  </section>
+</main>
+
+<style lang="scss">
+  main {
+    display: flex;
+    margin: 2rem 3rem 0 3rem;
+    justify-content: space-between;
+  }
+
+  .content {
+    width: 67%;
+  }
+
+  .actions {
+    border-left: 1px dotted #bfa38a;
+    padding-left: 2rem;
+  }
+</style>
